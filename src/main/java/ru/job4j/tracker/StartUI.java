45ea6fcb1287e46f8dc5220ed56a54d66367323c
Никита <nightwaymoon@gmail.com>
2022-10-16 04:21:14 +0300

    7. Date. Отображение даты. [#310017]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,14 @@
+package ru.job4j.tracker;
+
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+
+    public static void main(String[] args) {
+        Item currentTime = new Item();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String currentTimeFormat = currentTime.getTime().format(formatter);
+        System.out.println(currentTimeFormat);
+    }
+
+}
