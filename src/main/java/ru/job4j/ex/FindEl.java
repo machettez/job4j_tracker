45bb374d6113457f003027f94e04807229908f4f
Никита <nightwaymoon@gmail.com>
2022-11-03 19:12:29 +0300

    0.5. Иерархия исключений и множественный catch. [#219368] // findel and EAE

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -16,6 +16,20 @@ public class FindEl {
         return rsl;
     }
 
+    public static boolean sent(String value, String[] abuses) throws ElementAbuseException {
+        return true;
+    }
+
+    public static void process(String[] values, String key, String[] abuses) {
+        try {
+            if (indexOf(values, key) != 1) {
+                sent(key, abuses);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) throws ElementNotFoundException {
         String[] names = {"Petr", "Ivan", "Nikita", "Vasiliy"};
         try {
