9be3ea3c3a19a91db2015130087be0e857c417cb
Никита <nightwaymoon@gmail.com>
2022-11-03 19:07:56 +0300

    0.5. Иерархия исключений и множественный catch. [#219368]

---
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -0,0 +1,43 @@
+package ru.job4j.ex;
+
+public class UserStore {
+
+    public static User findUser(User[] users, String login) throws UserNotFoundException {
+        User newUser = null;
+        for (User user : users) {
+            if (login.equals(user.getUsername())) {
+                newUser = user;
+                break;
+            }
+        }
+        if (newUser == null) {
+            throw new UserNotFoundException("User not found");
+        }
+        return newUser;
+    }
+
+    public static boolean validate(User user) throws UserInvalidException {
+        if (!user.isValid() || user.getUsername().length() < 3) {
+            throw new UserInvalidException("User not valid");
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        User[] users = {
+                new User("Nikita", true)
+        };
+        try {
+            User user = findUser(users, "Nikita");
+            if (validate(user)) {
+                System.out.println("This user has an access");
+            }
+        } catch (UserInvalidException ie) {
+            ie.printStackTrace();
+        } catch (UserNotFoundException nfe) {
+            nfe.printStackTrace();
+        }
+
+    }
+
+}
