07323e8a5a8b54206ad452e6d5e33854bb53289c
Никита <nightwaymoon@gmail.com>
2022-11-03 20:00:24 +0300

    2.1. Тест на ValidateInput [#311887]

--- src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -43,7 +43,7 @@ public class ValidateInputTest {
     }
 
     @Test
-    public void whenNegativeInvalidInput() {
+    public void whenNegativeNumberValidInput() {
         Output out = new StubOutput();
         Input in = new StubInput(
                 new String[] {"-1"}
